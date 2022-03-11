54cc7544ba6d832c9c9c0e5862f1760a2b8f147e
legolas60007 <91493754+legolas60007@users.noreply.github.com>
2022-03-11 10:53:30 +0100

    Update 0-provider.tf

--- 0-provider.tf
+++ 0-provider.tf
@@ -1,6 +1,8 @@
 provider "aws" {
   region = "eu-west-3"
-}
+  access_key = "AKIATV4HRSSPDMDI6VOD"
+  secret_key = "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"
+  }
 
 terraform {
   required_providers {
